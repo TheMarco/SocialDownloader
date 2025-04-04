e53417043e7a95e4004e40a5705ad468c4bb0e47
Marco van Hylckama Vlieg <marcovhv@Marcos-MacBook-Pro.local>
2025-04-04 10:37:33 -0700

    port

--- run.py
+++ run.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     # from waitress import serve
     # serve(app, host='0.0.0.0', port=8080)
 
-    app.run(host='0.0.0.0', port=8000, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
